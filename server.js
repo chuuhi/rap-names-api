55237a3e798e2062b69e1fc825d3fa3afad26721
rayne <pothong.r@gmail.com>
2025-09-24 15:06:21 -0700

    added index.ejs

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 const cors = require('cors')
 const PORT = 8000
+require('dotenv').config()
 
 app.use(cors());
+
+let db,
+    dbConnectionStr = process.env.DB_STRING
+    dbName = 'rap'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to {dbName} Database`)
+        db = client.db(dbName)
+    })
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const rappers = {
     '21 savage': {
